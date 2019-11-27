6ec290988012981bfa93ea8900a61335e1f58ae8
usuryasatyakumar <usuryasatyakumar@gmail.com>
2019-11-27 15:28:38 +0530

    Rename to v2

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -40,7 +40,7 @@ function main() {
         -locations=filesystem:"$script_dir"/databases/tracker \
         -user="$db_username" \
         -password="$db_password" \
-        migrate
+        clean migrate
 
     echo "Migration complete"
 
